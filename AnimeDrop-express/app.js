4840987cbb654081dd018a5856cdcc96fed9541a
StevanyAngel <stevanyangel_2327240033@mhs.mdp.ac.id>
2025-12-11 11:20:43 +0700

    Update app.js

--- AnimeDrop-express/app.js
+++ AnimeDrop-express/app.js
@@ -1,8 +1,19 @@
 import express from "express";
+import api from "./routes/api.js";
+import database from "./config/database.js";
+import cors from "cors";
+
 const app = express();
 
+// Middleware
+app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use("/api", api);
+
+// Start Server
 app.listen("3000", () => {
+  database();
   console.log("🚀 AnimeDrop API berjalan di : http://localhost:3000");
 });
