1e4b5b5ec40d5460b5a11c4b3b7e131317b5913b
Obertsean111 <Oberts1310@gmail.com>
2025-12-12 21:17:15 +0700

    Update database

--- AnimeDrop-express/config/database.js
+++ AnimeDrop-express/config/database.js
@@ -4,7 +4,7 @@ const database = async () => {
   try {
     console.log("⏳ Koneksi ke database...");
 
-    const response = await mongoose.connect("mongodb://127.0.0.1:27017/animeDrop?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.8");
+    const response = await mongoose.connect("mongodb://127.0.0.1:27017/AnimeDrop?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.8");
 
     console.log(`✅ Koneksi database berhasil : ${response.connection.host}`);
   } catch (error) {
